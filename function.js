5b060ffdf91ddd767ebb9db9a28186819a8f6d2d
mackotomi <knowledgehole@gmail.com>
2025-03-20 13:57:25 +0100

    oke, uz naozaj idem lol .. :D

--- function.js
+++ function.js
@@ -1,9 +1,10 @@
 console.log(orders);
-// Filter all object elements within an array, where object element key === amount && createdAt && customerName
-let orderByFilter = orders.filter(order => 
-    'amount' in order && 
-    'createdAt' in order && 
-    'customerName' in order
-);
 
-console.log(orderByFilter);
+let oderdate = 1708272032000;
+let date_convertion = new Date(oderdate);
+
+console.log('this date 170827203200 has been converted into' + date_convertion);
+
+// Filter all array objects elements where state === canceled
+let cancelled_orders = orders.filter(order => order.state === 'canceled');
+console.log(cancelled_orders);
