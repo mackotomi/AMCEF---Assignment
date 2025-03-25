0707a5959487f3e751a6f578f7ea03cc7266ef50
mackotomi <knowledgehole@gmail.com>
2025-03-25 11:30:35 +0100

    latest update, probably done, maybe will do more changes if requested or required

--- function.js
+++ function.js
@@ -1,14 +1,6 @@
 // ≡Ö≡ Check
 console.log(orders);
 
-// Data conversion, obeyed, not gonna use this. Was curious to see the date in human readable format..
-let oderdate = 1708272032000;
-let date_convertion = new Date(oderdate);
-console.log('Checking the datatype of the new Date conversion, datatype is equal to -> ' + typeof date_convertion); // Makes an object
-
-
-console.log('this date 170827203200 has been converted into' + date_convertion);
-
 // Filter all array objects elements where state === canceled. The goal in the end is to get the cancelled orders → Highest priority eg. Top of the Hierrarchy so we begin focusing on the cnacelled orders.
 
 let cancelled_orders = orders.filter(order => order.state === 'canceled');
@@ -26,7 +18,7 @@ let canceled_orders_date_converstion = cancelled_orders.map(order => ({
 // Luckily, the dates are 
 
 // ≡Ö≡ Check
-console.log(canceled_orders_date_converstion);
+// console.log(canceled_orders_date_converstion);
 
 // Now lets clean the unnecessary properties from the cancelled orders, what we need in the end is only the year and the date of the order.
 canceled_orders_data_reduction = canceled_orders_date_converstion.map(order => {
@@ -35,7 +27,7 @@ canceled_orders_data_reduction = canceled_orders_date_converstion.map(order => {
 });
 
 // ≡Ö≡ Check
-console.log(canceled_orders_data_reduction);
+// console.log(canceled_orders_data_reduction);
 
 // Lastly, using if condition we create logical separation and filter the orders into new object, where key will be year, where year will hold corrensponding year/date of the orders.
 let canceled_orders_by_year = canceled_orders_data_reduction.reduce((acc, order) => {
@@ -47,4 +39,9 @@ let canceled_orders_by_year = canceled_orders_data_reduction.reduce((acc, order)
     return acc;
 }, {});
 
-console.log(canceled_orders_by_year);
+let object_objective = canceled_orders_by_year; // This references the object goal objective.
+
+// ≡Ö≡ Check
+console.dir(object_objective);
+
+// Final note, the object is ordered Asceding, so the first key is the oldest year, and the last key is the most recent year. This is by design as the The ECMAScript specification defines that numeric keys in plain objects are always enumerated in ascending order. Because of this, i would not recommend to change the order of the keys in the object if specifically requested with a purpose in mind. For this reason i didnt even tried to sort the object keys to start from 2024 ↓ 2023 ↓ 2022 ↓ 2021 ↓ 2020. You got the idea..
